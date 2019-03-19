07605a1f061cc1ad317e62813b5aca285e8ee7a8
jesslns <jesslns@yahoo.com.hk>
2019-03-19 16:16:07 +0000

    :white_check_mark: add test coverage for pose #bodyPartPosition

--- src/lib/posenetWrapper.test.js
+++ src/lib/posenetWrapper.test.js
@@ -19,5 +19,40 @@ it('#bodypartPosition can get leftWrist position', () => {
   expect(subject.bodypartPosition("leftWrist")).toEqual({x: 998.4647097518192, y: 498.58430285055033})
 });
 
+it('#bodypartPosition can get rightEye position', () => {
+  const input = sample['correct'][0]
+  const subject = new Pose(input)
+  expect(subject.bodypartPosition("rightEye")).toEqual({x: 849.8897991854535, y: 118.8806278152376})
+});
+
+it('#bodypartPosition can get leftEar position', () => {
+  const input = sample['correct'][0]
+  const subject = new Pose(input)
+  expect(subject.bodypartPosition("leftEar")).toEqual({x: 905.9810636207121, y: 131.71076510623712})
+});
+
+it('#bodypartPosition can get rightEar position', () => {
+  const input = sample['correct'][0]
+  const subject = new Pose(input)
+  expect(subject.bodypartPosition("rightEar")).toEqual({x: 833.1177493787358, y: 134.85277551082618})
+});
+
+it('#bodypartPosition can get leftShoulder position', () => {
+  const input = sample['correct'][0]
+  const subject = new Pose(input)
+  expect(subject.bodypartPosition("leftShoulder")).toEqual({x: 939.0765205748116, y: 242.1837124414523})
+});
+
+it('#bodypartPosition can get rightShoulder position', () => {
+  const input = sample['correct'][0]
+  const subject = new Pose(input)
+  expect(subject.bodypartPosition("rightShoulder")).toEqual({x: 793.9800185364151, y: 243.4584244906692})
+});
+
+it('#bodypartPosition can get leftElbow position', () => {
+  const input = sample['correct'][0]
+  const subject = new Pose(input)
+  expect(subject.bodypartPosition("leftElbow")).toEqual({x: 966.0234542497726, y: 371.36710985810515})
+});
 
 ///make bodypart hash
