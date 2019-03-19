8f7e6e865c2110c3efcc660b4a3f141e246ec7d9
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-03-19 15:33:06 +0000

    :under_construction: Add 'can create a pose class instance' green test

---
+++ src/lib/posenetWrapper.js
@@ -0,0 +1,14 @@
+class Pose{
+  constructor(posenet_object) {
+    this.source = posenet_object;
+  }
+};
+
+module.exports = Pose
+
+//class Rectangle {
+//  constructor(height, width) {
+//    this.height = height;
+//    this.width = width;
+//  }
+//}
--- src/lib/posenetWrapper.test.js
+++ src/lib/posenetWrapper.test.js
@@ -1,5 +1,11 @@
 import sample from '../../spec/posenet_sample/neutral_pose_testing';
+import Pose from './posenetWrapper';
 
-it('has a test', () => {
-  console.log(sample['incorrect'])
+it('can create a pose class', () => {
+  const input = sample['correct'][0]
+  const subject = new Pose(input)
+  expect(subject.source).toEqual(input)
 });
+
+
+///make bodypart hash
