7062aa4e7cfb758ed5ca38feee2eaeae0ee517b7
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-03-19 15:57:31 +0000

    :sparkles: Pose#bodypartPosition
    Co-authored-by: Jess Leung <jesslns@yahoo.com.hk>

--- src/lib/posenetWrapper.js
+++ src/lib/posenetWrapper.js
@@ -1,14 +1,18 @@
 class Pose{
   constructor(posenet_object) {
     this.source = posenet_object;
+    this._bodypartIndexLookup = {
+      "nose" : 0, "leftEye" : 1, "rightEye" : 2,
+      "leftEar" : 3, "rightEar" : 4, "leftShoulder" : 5,
+      "rightShoulder" : 6, "leftElbow" : 7, "rightElbow" : 8,
+      "leftWrist" : 9, "rightWrist" : 10, "leftHip" : 11,
+      "rightHip" : 12, "leftKnee" : 13, "rightKnee" : 14,
+      "leftAnkle" : 15, "rightAnkle" : 16
+    }
+  }
+  bodypartPosition(bodypart){
+    return this.source['keypoints'][this._bodypartIndexLookup[bodypart]]['position'];
   }
 };
 
 module.exports = Pose
-
-//class Rectangle {
-//  constructor(height, width) {
-//    this.height = height;
-//    this.width = width;
-//  }
-//}
--- src/lib/posenetWrapper.test.js
+++ src/lib/posenetWrapper.test.js
@@ -7,5 +7,17 @@ it('can create a pose class', () => {
   expect(subject.source).toEqual(input)
 });
 
+it('#bodypartPosition can get nose position', () => {
+  const input = sample['correct'][0]
+  const subject = new Pose(input)
+  expect(subject.bodypartPosition("nose")).toEqual({x: 867.082363200039, y: 139.34810966428796})
+});
+
+it('#bodypartPosition can get leftWrist position', () => {
+  const input = sample['correct'][0]
+  const subject = new Pose(input)
+  expect(subject.bodypartPosition("leftWrist")).toEqual({x: 998.4647097518192, y: 498.58430285055033})
+});
+
 
 ///make bodypart hash
