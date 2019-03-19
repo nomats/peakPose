2ffb776c7ec8b028f19a6a5f4451279ce360e44e
jesslns <jesslns@yahoo.com.hk>
2019-03-19 12:38:32 +0000

    :white_check_mark: add 3 pose samples

--- spec/posenet_sample/neutral_pose_testing.js
+++ spec/posenet_sample/neutral_pose_testing.js
@@ -1,5 +1,4 @@
-
-const wrongSample1 = {
+const mountain1 = {
   //https://www.yogajournal.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_1720/MTQ3MTUyNzM1MjQ1MzEzNDg2/mountainhp2_292_37362_cmyk.webp
   score: 0.9374673366546631,
   keypoints: [
@@ -23,4 +22,76 @@ const wrongSample1 = {
   ]
 };
 
-module.exports = wrongSample1;
+const mountain2 = {
+  // https://3i133rqau023qjc1k3txdvr1-wpengine.netdna-ssl.com/wp-content/uploads/2016/03/Yoga_Mountain-Pose_Exercise-2.jpg
+  score: 0.9393985481823192,
+  keypoints: [
+    {score: 0.9949293732643127, part: "nose", position: {x: 120.85647457248562, y: 77.18718334636857}},
+    {score: 0.9946432113647461, part: "leftEye", position: {x: 132.08375071431254, y: 64.34899976612192}},
+    {score: 0.9901210069656372, part: "rightEye", position: {x: 107.07546229938885, y: 69.57842176994392}},
+    {score: 0.6708736419677734, part: "leftEar", position: {x: 146.00823454804473, y: 75.36387418223693}},
+    {score: 0.7917882800102234, part: "rightEar", position: {x: 94.6805170918559, y: 70.82650621802406}},
+    {score: 0.9984951019287109, part: "leftShoulder", position: {x: 162.9128338321225, y: 148.841501421633}},
+    {score: 0.9956321120262146, part: "rightShoulder", position: {x: 79.24812065376031, y: 149.40684387747166}},
+    {score: 0.9823945760726929, part: "leftElbow", position: {x: 182.2162801721594, y: 222.74201222647608}},
+    {score: 0.9681554436683655, part: "rightElbow", position: {x: 65.8323967902215, y: 230.35827231618154}},
+    {score: 0.962847888469696, part: "leftWrist", position: {x: 202.7510246025337, y: 319.9858193186532}},
+    {score: 0.9543367028236389, part: "rightWrist", position: {x: 42.456236493456494, y: 324.75146106281113}},
+    {score: 0.9760878086090088, part: "leftHip", position: {x: 150.93895151326944, y: 303.0178627756845}},
+    {score: 0.9908208250999451, part: "rightHip", position: {x: 91.36842899532108, y: 308.98432989880047}},
+    {score: 0.9603991508483887, part: "leftKnee", position: {x: 154.35161942702075, y: 451.32067168919383}},
+    {score: 0.984632134437561, part: "rightKnee", position: {x: 105.02874869042701, y: 453.50353220711764}},
+    {score: 0.878981351852417, part: "leftAnkle", position: {x: 132.3026451278519, y: 562.406811469424}},
+    {score: 0.874636709690094, part: "rightAnkle", position: {x: 117.37911274668934, y: 565.1369391247234}},
+  ]
+};
+
+const goddess1 = {
+  // https://i2.wp.com/www.feelgoodyogavictoria.com/fgyp/wp-content/uploads/2015/01/goddess-pose-deviasana.png?zoom=2&resize=376%2C396
+  score: 0.8067714589483598,
+  keypoints: [
+    {score: 0.9885653853416443, part: "nose", position: {x: 313.5378910433095, y: 149.70232754066345}}
+    {score: 0.9930859804153442, part: "leftEye", position: {x: 329.61143769356323, y: 135.6873429595447}}
+    {score: 0.9864208698272705, part: "rightEye", position: {x: 297.7349576860945, y: 135.78329615358447}}
+    {score: 0.8522735238075256, part: "leftEar", position: {x: 344.7119409674053, y: 145.61195300993373}}
+    {score: 0.5619542002677917, part: "rightEar", position: {x: 284.36341710551136, y: 146.73185449819096}}
+    {score: 0.9670693278312683, part: "leftShoulder", position: {x: 364.3139215392116, y: 226.3991557136911}}
+    {score: 0.9734305739402771, part: "rightShoulder", position: {x: 263.5299365082263, y: 208.68862249655803}}
+    {score: 0.9091839790344238, part: "leftElbow", position: {x: 471.7371297105451, y: 160.5766712126185}}
+    {score: 0.8775482177734375, part: "rightElbow", position: {x: 152.4275840212623, y: 160.40436146220225}}
+    {score: 0.2924434244632721, part: "leftWrist", position: {x: 141.10808494306428, y: 61.00912830790536}}
+    {score: 0.5988689064979553, part: "rightWrist", position: {x: 144.23210250997099, y: 60.3763734973845}}
+    {score: 0.9472538232803345, part: "leftHip", position: {x: 362.84028036052194, y: 413.3343275726819}}
+    {score: 0.9489489197731018, part: "rightHip", position: {x: 271.9970390343592, y: 422.4259206803119}}
+    {score: 0.8498261570930481, part: "leftKnee", position: {x: 513.5136606567003, y: 475.6993641837699}}
+    {score: 0.68251633644104, part: "rightKnee", position: {x: 513.5431885050837, y: 476.5147185779009}}
+    {score: 0.709509551525116, part: "leftAnkle", position: {x: 561.9560953776042, y: 633.6952253498015}}
+    {score: 0.5762156248092651, part: "rightAnkle", position: {x: 77.75166345756745, y: 636.0843527872055}}
+  ]
+};
+
+const handsOnHips1 = {
+  // https://www.dhresource.com/600x600/f2/albu/g6/M01/2A/74/rBVaSFu8DP6AGJl6AAICst4js4w436.jpg
+  score: 0.9444622222115012,
+  keypoints: [
+    0: {score: 0.9980316758155823, part: "nose", position: {x: 297.65605992511894, y: 69.04847036183499}}
+    1: {score: 0.9948192238807678, part: "leftEye", position: {x: 309.13835677308606, y: 55.663294280689065}}
+    2: {score: 0.9929817914962769, part: "rightEye", position: {x: 283.0801993505352, y: 55.873033992146944}}
+    3: {score: 0.8091188073158264, part: "leftEar", position: {x: 324.97710653654843, y: 64.2217444713553}}
+    4: {score: 0.8415245413780212, part: "rightEar", position: {x: 272.13714494012214, y: 67.16588954199555}}
+    5: {score: 0.9866852760314941, part: "leftShoulder", position: {x: 342.3489686114565, y: 126.42336518706746}}
+    6: {score: 0.9926885366439819, part: "rightShoulder", position: {x: 248.84692129379323, y: 126.17020920486185}}
+    7: {score: 0.9886897206306458, part: "leftElbow", position: {x: 400.87733285237346, y: 199.0904336157142}}
+    8: {score: 0.9841790795326233, part: "rightElbow", position: {x: 193.20959599372836, y: 192.8676671222832}}
+    9: {score: 0.9471879601478577, part: "leftWrist", position: {x: 354.20283017274005, y: 248.57322930464693}}
+    10: {score: 0.9548919200897217, part: "rightWrist", position: {x: 232.6720716219047, y: 240.15573151796332}}
+    11: {score: 0.9776188135147095, part: "leftHip", position: {x: 329.0292812466209, y: 276.8714667191555}}
+    12: {score: 0.9759510159492493, part: "rightHip", position: {x: 261.47397579206313, y: 281.18800390962906}}
+    13: {score: 0.9228923320770264, part: "leftKnee", position: {x: 329.1594825401438, y: 424.9675183147707}}
+    14: {score: 0.9694175720214844, part: "rightKnee", position: {x: 255.2255175105428, y: 415.4939644889435}}
+    15: {score: 0.8908283114433289, part: "leftAnkle", position: {x: 330.4288342749783, y: 549.8685104211721}}
+    16: {score: 0.8283511996269226, part: "rightAnkle", position: {x: 246.65505696332988, y: 548.4341420104347}}
+  ]
+};
+
+module.exports = mountain1;
