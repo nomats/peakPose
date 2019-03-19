23c0e5fd22d72f9f71a92345bc454f71d606442c
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-03-19 11:38:42 +0000

    :white_check_mark: :building_construction: add posenet sample data convention

---
+++ spec/posenet_sample/neutral_pose_testing.js
@@ -0,0 +1,26 @@
+
+const wrongSample1 = {
+  //https://www.yogajournal.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_1720/MTQ3MTUyNzM1MjQ1MzEzNDg2/mountainhp2_292_37362_cmyk.webp
+  score: 0.9374673366546631,
+  keypoints: [
+    {score: 0.9984795451164246, part: "nose", position: {x: 867.082363200039, y: 139.34810966428796}},
+    {score: 0.9976953268051147, part: "leftEye", position: {x: 884.5536936801585, y: 120.80305946448948}},
+    {score: 0.995785653591156, part: "rightEye", position: {x: 849.8897991854535, y: 118.8806278152376}},
+    {score: 0.8357583284378052, part: "leftEar", position: {x: 905.9810636207121, y: 131.71076510623712}},
+    {score: 0.8865057826042175, part: "rightEar", position: {x: 833.1177493787358, y: 134.85277551082618}},
+    {score: 0.9934952259063721, part: "leftShoulder", position: {x: 939.0765205748116, y: 242.1837124414523}},
+    {score: 0.9838179349899292, part: "rightShoulder", position: {x: 793.9800185364151, y: 243.4584244906692}},
+    {score: 0.9866327047348022, part: "leftElbow", position: {x: 966.0234542497726, y: 371.36710985810515}},
+    {score: 0.989894688129425, part: "rightElbow", position: {x: 764.4417666238955, y: 370.7695001932421}},
+    {score: 0.9488635063171387, part: "leftWrist", position: {x: 998.4647097518192, y: 498.58430285055033}},
+    {score: 0.8745731115341187, part: "rightWrist", position: {x: 730.0744491862656, y: 504.11775658633326}},
+    {score: 0.9217945337295532, part: "leftHip", position: {x: 914.5591358335499, y: 481.72699298117277}},
+    {score: 0.9597892761230469, part: "rightHip", position: {x: 810.6170763424182, y: 486.88238101235834}},
+    {score: 0.9720034003257751, part: "leftKnee", position: {x: 899.8785408004159, y: 702.4112093041447}},
+    {score: 0.9774855375289917, part: "rightKnee", position: {x: 824.2542163745777, y: 703.2182234729278}},
+    {score: 0.8457183837890625, part: "leftAnkle", position: {x: 880.5404146634615, y: 882.1650988755154}},
+    {score: 0.7686517834663391, part: "rightAnkle", position: {x: 849.9319304468231, y: 882.6055469670201}},
+  ]
+};
+
+module.exports = wrongSample1;
