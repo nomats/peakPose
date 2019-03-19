31bbcd468d8a9aea7cec0464ca912c3599a28d1f
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-03-19 16:11:26 +0000

    :white_check_mark: add more test coverage for Pose#bodypartPosition

--- src/lib/posenetWrapper.test.js
+++ src/lib/posenetWrapper.test.js
@@ -19,5 +19,47 @@ it('#bodypartPosition can get leftWrist position', () => {
   expect(subject.bodypartPosition("leftWrist")).toEqual({x: 998.4647097518192, y: 498.58430285055033})
 });
 
+it('#bodypartPosition can get rightWrist position', () => {
+  const input = sample['correct'][0]
+  const subject = new Pose(input)
+  expect(subject.bodypartPosition("rightWrist")).toEqual({x: 730.0744491862656, y: 504.11775658633326})
+});
+
+it('#bodypartPosition can get leftHip position', () => {
+  const input = sample['correct'][0]
+  const subject = new Pose(input)
+  expect(subject.bodypartPosition("leftHip")).toEqual({x: 914.5591358335499, y: 481.72699298117277})
+});
+
+it('#bodypartPosition can get rightHip position', () => {
+  const input = sample['correct'][0]
+  const subject = new Pose(input)
+  expect(subject.bodypartPosition("rightHip")).toEqual({x: 810.6170763424182, y: 486.88238101235834})
+});
+
+it('#bodypartPosition can get leftKnee position', () => {
+  const input = sample['correct'][0]
+  const subject = new Pose(input)
+  expect(subject.bodypartPosition("leftKnee")).toEqual({x: 899.8785408004159, y: 702.4112093041447})
+});
+
+it('#bodypartPosition can get rightKnee position', () => {
+  const input = sample['correct'][0]
+  const subject = new Pose(input)
+  expect(subject.bodypartPosition("rightKnee")).toEqual({x: 824.2542163745777, y: 703.2182234729278})
+});
+
+it('#bodypartPosition can get leftAnkle position', () => {
+  const input = sample['correct'][0]
+  const subject = new Pose(input)
+  expect(subject.bodypartPosition("leftAnkle")).toEqual({x: 880.5404146634615, y: 882.1650988755154})
+});
+
+it('#bodypartPosition can get rightAnkle position', () => {
+  const input = sample['correct'][0]
+  const subject = new Pose(input)
+  expect(subject.bodypartPosition("rightAnkle")).toEqual( {x: 849.9319304468231, y: 882.6055469670201})
+});
+
 
 ///make bodypart hash
