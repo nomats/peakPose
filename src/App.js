65a1d8564c833096d57a1e89abb25f242ba79bde
ellie turnock <ebturnock@gmail.com>
2019-03-21 11:32:34 +0000

    :hankey: videoFeed component tests need updating, cypress deleted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import VideoFeed from "./components/videoFeed";
 
@@ -7,18 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>asanaApp lives here</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        Here's the webcam
         <VideoFeed/>
       </div>
     );
---
+++ src/components/videoFeed/videoFeed.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import VideoFeed from './index.js';
+import { shallow } from 'enzyme';
+
+xit('mounts the video feed', () => {
+  expect(mediaDevices.getUserMedia.calledOnce).toBe(true)
+});
