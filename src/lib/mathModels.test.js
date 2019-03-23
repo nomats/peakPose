b9924f786b2d87603d0d43ae79cafa5f359779cc
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-03-23 17:05:17 +0000

    :white_check_mark: added test for MathModel#angle

--- src/lib/mathModels.test.js
+++ src/lib/mathModels.test.js
@@ -2,12 +2,17 @@ import MathModels from './mathModels';
 
 
 
-it('can calculate angles correctly', () => {
+it('MathModels#angle can calculate angles correctly', () => {
   var _mm = new MathModels()
   expect(_mm.angle({x: 12, y: 5},{x: 0, y: 0},{x: 12, y: 0})).toEqual(23)
 });
 
-it('can calculate angles correctly', () => {
+it('MathModels#angle can calculate angles correctly', () => {
   var _mm = new MathModels()
   expect(_mm.angle({x: 0, y: 2},{x: 5, y: 5},{x: 2, y: 0})).toEqual(28)
+});
+
+it('MathModels#angle can calculate angles correctly', () => {
+  var _mm = new MathModels()
+  expect(_mm.angle({x: 2, y: 0},{x: 0, y: 2},{x: 5, y: 5})).toEqual(76)
 });
