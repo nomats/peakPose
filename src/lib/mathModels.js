24f85620b8222710f451f3aca0c9b9f0f3612ce5
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-03-23 17:00:05 +0000

    :sparkles: add full MathModels#angle

--- src/lib/mathModels.js
+++ src/lib/mathModels.js
@@ -2,8 +2,12 @@ class MathModels{
   constructor() {
     };
 
-  angle(edge_1, middle, edge_2){
-    return 22.6
+  angle(p_edge1, p_middle, p_edge2){
+    //Note: rounds to one degree
+    var a = Math.sqrt((p_edge1['x']-p_middle['x'])**2 + (p_edge1['y']-p_middle['y'])**2)
+    var b = Math.sqrt((p_edge2['x']-p_middle['x'])**2 + (p_edge2['y']-p_middle['y'])**2)
+    var c = Math.sqrt((p_edge2['x']-p_edge1['x'])**2 + (p_edge2['y']-p_edge1['y'])**2)
+    return Math.round(Math.acos((a**2 + b**2 - c**2)/(2*a*b))*(360/(Math.PI * 2)))
   }
 }
 
--- src/lib/mathModels.test.js
+++ src/lib/mathModels.test.js
@@ -4,5 +4,10 @@ import MathModels from './mathModels';
 
 it('can calculate angles correctly', () => {
   var _mm = new MathModels()
-  expect(_mm.angle({x: 12, y: 5},{x: 0, y: 0},{x: 12, y: 0})).toEqual(22.6)
+  expect(_mm.angle({x: 12, y: 5},{x: 0, y: 0},{x: 12, y: 0})).toEqual(23)
+});
+
+it('can calculate angles correctly', () => {
+  var _mm = new MathModels()
+  expect(_mm.angle({x: 0, y: 2},{x: 5, y: 5},{x: 2, y: 0})).toEqual(28)
 });
