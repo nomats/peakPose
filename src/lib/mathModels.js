a84d56b42b4e96e6484af82b2835ae6a65734051
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-03-24 18:16:34 +0000

    :sparkles: improved MathModels#isStraight

--- src/lib/mathModels.js
+++ src/lib/mathModels.js
@@ -13,12 +13,31 @@ class MathModels{
     return Math.round(Math.acos((a**2 + b**2 - c**2)/(2*a*b))*(360/(Math.PI * 2)))
   }
 
-  isStraight(points, margin=0.9){
-    var goodness = this._regHandler.coefficientOfDetermination(points)
-    console.log("goodness:")
-    console.log(goodness)
-    console.log((goodness > margin))
-    return (goodness > margin)
+  isStraight(points, margin=10){
+    var result = true
+    var b_1 = this._regHandler.b_1(points)
+    var b_0 = this._regHandler.b_0(points, b_1)
+    var b_1Inverted = -1*(1/b_1)
+    const yHat = this._regHandler.yHat
+    for(var i = 0; i < points.length; i++){
+      var c_o = points[i]['y'] + points[i]['x']/b_1
+      var x_i = (b_1*(c_o - b_0))/((b_1**2)+1)
+      var y_i = yHat(x_i,b_1,b_0)
+      var distance = Math.sqrt((points[i]['y']-y_i)**2 + (points[i]['x']-x_i)**2)
+      if(distance > margin ){
+        result = false
+      }
+      console.log("intercept")
+      console.log(distance>margin)
+      console.log("distance")
+      console.log(distance)
+      console.log(x_i)
+      console.log(y_i)
+      console.log("original coordinate")
+      console.log(points[i]['x'])
+      console.log(points[i]['y'])
+    }
+    return result
   }
   isHorizontal(points, margin=5){
     var result = points.length < 2 ? false : true
--- src/lib/mathModels.test.js
+++ src/lib/mathModels.test.js
@@ -19,13 +19,13 @@ it('MathModels#angle can calculate angles correctly', () => {
 
 it('MathModels#isStraight can deduce straight lines', () => {
   var _mm = new MathModels()
-  expect(_mm.isStraight([{x: 2, y: 0},{x: 0, y: 2},{x: 5, y: 5}])).toEqual(false)
+  expect(_mm.isStraight([{x: 2, y: 0},{x: 0, y: 2},{x: 5, y: 5}],1)).toEqual(false)
 });
 
 it('MathModels#isStraight can deduce straight lines', () => {
   var _mm = new MathModels()
   expect(_mm.isStraight([{x: 1, y: -0.5},{x: 3,y: -0.6},{x: 5,y: -0.7},{x: 7,y: -0.8},
-     {x: 9, y:-0.9}, {x: 11, y:-1.0}])).toEqual(true)
+     {x: 9, y:-0.9}, {x: 11, y:-1.0}],1)).toEqual(true)
 });
 
 it('MathModels#isStraight can deduce straight lines', () => {
@@ -36,15 +36,14 @@ it('MathModels#isStraight can deduce straight lines', () => {
 
 it('MathModels#isStraight can deduce straight lines', () => {
   var _mm = new MathModels()
-  console.log("ourguy")
   expect(_mm.isStraight([{x: -1, y: 3},{x: 1,y: 3.1},{x: 3,y: 3.6},{x: 5,y: 3.3},
-     {x: 7, y:2.9}, {x: 9, y:3.1}])).toEqual(true)
+     {x: 7, y:2.9}, {x: 9, y:3.1}],1)).toEqual(true)
 });
 
 it('MathModels#isStraight can deduce straight lines', () => {
   var _mm = new MathModels()
   expect(_mm.isStraight([{x: -1, y: -9},{x: -1.01,y: 5},{x: -1.02,y: -3.7},{x: 1.03,y: -0.8},
-     {x: 13.04, y:-0.9}, {x: -1.05, y:-1.0}])).toEqual(false)
+     {x: 13.04, y:-0.9}, {x: -1.05, y:-1.0}],4)).toEqual(false)
 });
 
 it('MathModels#isHorizontal can deduce horizontal lines', () => {
