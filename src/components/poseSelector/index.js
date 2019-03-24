fe74497ee151de43cba1c8a9887db0772f79dade
ellie turnock <ebturnock@gmail.com>
2019-03-24 18:51:33 +0000

    :sparkles: basic poseSelector component to test data flow

---
+++ src/components/poseSelector/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+class PoseSelector extends React.Component {
+
+  render() {
+    return (
+      <div>poseSelector rendering</div>
+    );
+  }
+}
+
+const StyledPoseSelector = styled(PoseSelector)`
+  border: 5px solid palevioletred;
+`
+;
+
+export default StyledPoseSelector;
