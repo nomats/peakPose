7ac4114b0b7603cbf73dabb37e08fa79505965d1
William Dunk <williamdunk@gmail.com>
2019-03-25 12:25:10 +0000

    :white_check_mark: first checklist test

---
+++ src/lib/poseChecklist.test.js
@@ -0,0 +1,11 @@
+import sample from "../../spec/posenet_sample/warrior2_pose_testing";
+import Pose from "./posenetWrapper";
+
+it("#isWarrior2 can recognise correct position", () => {
+  var input = sample["correct"][0];
+  var subject = new Pose(input);
+  expect(subject.isWarrior2()[1]).toEqual([
+    ["Horizontal Arms", true],
+    ["Knee at 90", true]
+  ]);
+});
