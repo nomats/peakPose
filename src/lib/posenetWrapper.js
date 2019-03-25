eaee9d2dc711a8b38eec3ec93a5a60567f2074af
jesslns <jesslns@yahoo.com.hk>
2019-03-25 17:39:29 +0000

    :bug: removed unsued cariable in isMountainPose to pass Travis CI

--- src/lib/posenetWrapper.js
+++ src/lib/posenetWrapper.js
@@ -20,23 +20,21 @@ class Pose{
   }
 
   isMountainPose(){
-    // var conditional =  new conditional([
-    //this._isPointBetween(blah),
-    //this._isHigherThan(blah),
-    //this._isLowerThan(blah),
-    //])
-    // return conditional.isMet
-    const c1 = this.bodypart("rightWrist").position['y'] > this.bodypart("rightHip").position['y']
-    const c2 = this.bodypart("leftWrist").position['y'] > this.bodypart("leftHip").position['y']
-    // lower body x-coordinates
-    const c3 = this._isPointBetween(this.bodypart("leftAnkle").position['x'],[this.bodypart("leftShoulder").position['x'],this.bodypart("rightShoulder").position['x']])
-    const c4 = this._isPointBetween(this.bodypart("rightAnkle").position['x'],[this.bodypart("leftShoulder").position['x'],this.bodypart("rightShoulder").position['x']])
-    const c5 = this.bodypart("leftAnkle").position['x'] > this.bodypart("rightAnkle").position['x']
-    const c6 = this._isStraight([this.bodypart("leftAnkle").position,
-                                this.bodypart("leftKnee").position,
-                                this.bodypart("leftHip").position])
-    return (c1 && c2 && c3 && c4 && c5 )
-  }
+  // var conditional =  new conditional([
+  //this._isPointBetween(blah),
+  //this._isHigherThan(blah),
+  //this._isLowerThan(blah),
+  //])
+  // return conditional.isMet
+  const c1 = this.bodypart("rightWrist").position['y'] > this.bodypart("rightHip").position['y']
+  const c2 = this.bodypart("leftWrist").position['y'] > this.bodypart("leftHip").position['y']
+  // lower body x-coordinates
+  const c3 = this._isPointBetween(this.bodypart("leftAnkle").position['x'],[this.bodypart("leftShoulder").position['x'],this.bodypart("rightShoulder").position['x']])
+  const c4 = this._isPointBetween(this.bodypart("rightAnkle").position['x'],[this.bodypart("leftShoulder").position['x'],this.bodypart("rightShoulder").position['x']])
+  const c5 = this.bodypart("leftAnkle").position['x'] > this.bodypart("rightAnkle").position['x']
+
+  return (c1 && c2 && c3 && c4 && c5 )
+}
 
 isGoddess(){
   const criteria1 = "both knees are bend and hips are open";
