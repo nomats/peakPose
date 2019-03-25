c1928683b0b9990fe57d4758724a66f8667c73c3
jesslns <jesslns@yahoo.com.hk>
2019-03-25 12:31:33 +0000

    :sparkles: Add goddess pose correct samples

---
+++ spec/posenet_sample/goddess_pose_testing.js
@@ -0,0 +1,71 @@
+const goddess1 = {
+  // https://i2.wp.com/www.feelgoodyogavictoria.com/fgyp/wp-content/uploads/2015/01/goddess-pose-deviasana.png?zoom=2&resize=376%2C396
+  score: 0.8067714589483598,
+  keypoints: [
+    {score: 0.9885653853416443, part: "nose", position: {x: 313.5378910433095, y: 149.70232754066345}},
+    {score: 0.9930859804153442, part: "leftEye", position: {x: 329.61143769356323, y: 135.6873429595447}},
+    {score: 0.9864208698272705, part: "rightEye", position: {x: 297.7349576860945, y: 135.78329615358447}},
+    {score: 0.8522735238075256, part: "leftEar", position: {x: 344.7119409674053, y: 145.61195300993373}},
+    {score: 0.5619542002677917, part: "rightEar", position: {x: 284.36341710551136, y: 146.73185449819096}},
+    {score: 0.9670693278312683, part: "leftShoulder", position: {x: 364.3139215392116, y: 226.3991557136911}},
+    {score: 0.9734305739402771, part: "rightShoulder", position: {x: 263.5299365082263, y: 208.68862249655803}},
+    {score: 0.9091839790344238, part: "leftElbow", position: {x: 471.7371297105451, y: 160.5766712126185}},
+    {score: 0.8775482177734375, part: "rightElbow", position: {x: 152.4275840212623, y: 160.40436146220225}},
+    {score: 0.2924434244632721, part: "leftWrist", position: {x: 141.10808494306428, y: 61.00912830790536}},
+    {score: 0.5988689064979553, part: "rightWrist", position: {x: 144.23210250997099, y: 60.3763734973845}},
+    {score: 0.9472538232803345, part: "leftHip", position: {x: 362.84028036052194, y: 413.3343275726819}},
+    {score: 0.9489489197731018, part: "rightHip", position: {x: 271.9970390343592, y: 422.4259206803119}},
+    {score: 0.8498261570930481, part: "leftKnee", position: {x: 513.5136606567003, y: 475.6993641837699}},
+    {score: 0.68251633644104, part: "rightKnee", position: {x: 513.5431885050837, y: 476.5147185779009}},
+    {score: 0.709509551525116, part: "leftAnkle", position: {x: 561.9560953776042, y: 633.6952253498015}},
+    {score: 0.5762156248092651, part: "rightAnkle", position: {x: 77.75166345756745, y: 636.0843527872055}}
+  ]
+};
+
+const goddess2 = {
+  // https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKpMjjV_6fagQt0jhdrqLo1HDKQ5uk09TVyLEj988hXTpUvydG
+  score: 0.8491435752195471,
+  keypoints: [
+    {score: 0.9647425413131714, part: "nose", position: {x: 123.75815421035608, y: 21.964038494413934}},
+    {score: 0.869419515132904, part: "leftEye", position: {x: 128.0955065304471, y: 13.443019537799128}},
+    {score: 0.9290948510169983, part: "rightEye", position: {x: 113.92267588979189, y: 13.981573180814761}},
+    {score: 0.5198217630386353, part: "leftEar", position: {x: 133.42305857864852, y: 20.46608435369171}},
+    {score: 0.7159637808799744, part: "rightEar", position: {x: 110.89271631929063, y: 27.308045910523003}},
+    {score: 0.961938738822937, part: "leftShoulder", position: {x: 135.29730350455057, y: 53.15649665562453}},
+    {score: 0.9909741878509521, part: "rightShoulder", position: {x: 96.37412808605076, y: 60.12232645423012}},
+    {score: 0.9225767850875854, part: "leftElbow", position: {x: 157.46514609425338, y: 74.8949351352928}},
+    {score: 0.9185812473297119, part: "rightElbow", position: {x: 77.27572568913095, y: 106.75452612142648}},
+    {score: 0.7236381769180298, part: "leftWrist", position: {x: 156.36133111383495, y: 136.90829623062}},
+    {score: 0.8791919350624084, part: "rightWrist", position: {x: 78.41880436533505, y: 152.31674194335938}},
+    {score: 0.9716985821723938, part: "leftHip", position: {x: 139.90429545923607, y: 138.5966248638862}},
+    {score: 0.9557982087135315, part: "rightHip", position: {x: 108.06339826288911, y: 141.27632985072853}},
+    {score: 0.931568443775177, part: "leftKnee", position: {x: 157.8732158228294, y: 134.35492794070623}},
+    {score: 0.8560307025909424, part: "rightKnee", position: {x: 71.30186438806278, y: 133.99392659685253}},
+    {score: 0.6583778262138367, part: "leftAnkle", position: {x: 153.81421118667444, y: 184.46567805467456}},
+    {score: 0.6660234928131104, part: "rightAnkle", position: {x: 97.50421441461621, y: 205.43905038749224}},
+  ]
+};
+
+const goddess3 = {
+  // https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BBfcbKX/_h400_w284_m1_bwhite.jpg
+  score: 0.6917345295934116,
+  keypoints: [
+    0: {score: 0.8886802792549133, part: "nose", position: {x: 126.21708212738827, y: 181.69610075248303}},
+    1: {score: 0.8275051116943359, part: "leftEye", position: {x: 131.39031761050842, y: 177.01281761938287}},
+    2: {score: 0.8514989018440247, part: "rightEye", position: {x: 128.28988188906655, y: 175.68253509758054}},
+    3: {score: 0.561092734336853, part: "leftEar", position: {x: 146.04637659893135, y: 180.4844854783642}},
+    4: {score: 0.3592619299888611, part: "rightEar", position: {x: 111.22523490629047, y: 178.83824406113735}},
+    5: {score: 0.8980261087417603, part: "leftShoulder", position: {x: 136.55335421389248, y: 195.33497347572975}},
+    6: {score: 0.8471747040748596, part: "rightShoulder", position: {x: 118.76919603100711, y: 202.5942512926205}},
+    7: {score: 0.6728863716125488, part: "leftElbow", position: {x: 149.16584232310555, y: 207.87293663320614}},
+    8: {score: 0.5455041527748108, part: "rightElbow", position: {x: 84.86347099659973, y: 258.7884923654009}},
+    9: {score: 0.2988691031932831, part: "leftWrist", position: {x: 149.31491990163536, y: 197.60093842735586}},
+    10: {score: 0.5197473764419556, part: "rightWrist", position: {x: 106.59770273791693, y: 122.5069351787715}},
+    11: {score: 0.7847316265106201, part: "leftHip", position: {x: 140.703365345693, y: 265.2183591857437}},
+    12: {score: 0.724550187587738, part: "rightHip", position: {x: 114.91477057106135, y: 265.03801520295843}},
+    13: {score: 0.7646034955978394, part: "leftKnee", position: {x: 172.36446716624837, y: 276.1365186114644}},
+    14: {score: 0.8654268980026245, part: "rightKnee", position: {x: 85.91782169638519, y: 273.974893496018}},
+    15: {score: 0.5868241786956787, part: "leftAnkle", position: {x: 173.615213502874, y: 341.07597315588663}},
+    16: {score: 0.7631038427352905, part: "rightAnkle", position: {x: 64.15093772769592, y: 336.72736021529795}}
+  ]
+};
