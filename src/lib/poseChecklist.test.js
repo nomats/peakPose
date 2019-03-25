3ec4d835304b6fe31be045bb83d9a6136932f6c3
William Dunk <williamdunk@gmail.com>
2019-03-25 14:28:48 +0000

    :white_check_mark: add more tests for pose checklist
    Co-authored-by: Luca <lfruzza@hotmail.co.uk>

--- src/lib/poseChecklist.test.js
+++ src/lib/poseChecklist.test.js
@@ -1,11 +1,29 @@
 import sample from "../../spec/posenet_sample/warrior2_pose_testing";
 import Pose from "./posenetWrapper";
 
-it("#isWarrior2 can recognise correct position", () => {
+it("#isWarrior2 returns message list for correct pose", () => {
   var input = sample["correct"][0];
   var subject = new Pose(input);
   expect(subject.isWarrior2()[1]).toEqual([
     [true, "Horizontal Arms"],
     [true, "Knee at 90"]
+  ]);
+});
+
+it("#isWarrior2 returns message list for incorrect pose", () => {
+  var input = sample["nearly"][0];
+  var subject = new Pose(input);
+  expect(subject.isWarrior2()[1]).toEqual([
+    [false, "Horizontal Arms"],
+    [true, "Knee at 90"]
+  ]);
+});
+
+it("#isWarrior2 returns message list for incorrect pose", () => {
+  var input = sample["nearly"][1];
+  var subject = new Pose(input);
+  expect(subject.isWarrior2()[1]).toEqual([
+    [true, "Horizontal Arms"],
+    [false, "Knee at 90"]
   ]);
 });
