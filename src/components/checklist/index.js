6a3ba5e3a23ad402f5a1106d9a6a431dde4f8837
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-03-26 11:28:29 +0000

    :art: refactored components

--- src/components/checklist/index.js
+++ src/components/checklist/index.js
@@ -1,39 +1,46 @@
 import React from "react";
 import styled from "styled-components";
 
-class Checklist extends React.Component {
+const Criteria = (props) => {
+  return <li class={props.check.toString()}> {props.check[1]} </li>;
+};
 
-  constructor(props) {
-    super(props);
-    this.result = [false,[[true,"arms spaghetti"],[false, "darmsspaghetti"]]];
-    this.checks = this.result[1]
-  }
+const StyledCriteria = styled(Criteria)`
+.false{
+  color: red;
+}
+.true{
+  color: green;
+}
+`
 
-  render(){
-    return (
-      <div className={this.props.className}>
-      <ul>
-      {this.checks.map((check, index)=>{
-        return (
-          <li class={check[0].toString()}> {check[1]} </li>
-        )
-      })}
-      </ul>
-      </div>
-    )
-  }
-
+const Checklist = (props) => {
+  return (
+    <div className={props.className}>
+    <ul>
+    {props.checks.map((check_i, index)=>{
+      return (
+        <StyledCriteria check={check_i} />
+      )
+    })}
+    </ul>
+    </div>
+  )
 }
 
 
+export default Checklist;
 
-const StyledChecklist = styled(Checklist)`
-  li.false{
-    color: red;
-  }
-  li.true{
-    color: green;
-  }
-`
-
-export default StyledChecklist;
+///
+///
+/// prospectve model:
+/// class main(){
+//  afterMount({
+//    setInterval
+//    setstate(new pose biz)
+//  })
+//  }
+//  render(){
+//    <Checklist
+//  }
+//
