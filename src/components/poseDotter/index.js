2a2832286f98769de27c998d4b2b96634d734823
William Dunk <williamdunk@gmail.com>
2019-03-28 21:05:45 +0000

    :green_heart: remove unused variables

--- src/components/poseDotter/index.js
+++ src/components/poseDotter/index.js
@@ -1,14 +1,10 @@
 import React from "react";
-import * as posenet from "@tensorflow-models/posenet";
 import styled from "styled-components";
 
 class PoseDotter extends React.Component {
   componentDidMount() {
     const canvas = this.refs.canvas;
     const ctx = canvas.getContext("2d");
-    const img = this.refs.image;
-    const video = document.getElementsByTagName("video")[0];
-    var source = this.props.pose;
     setInterval(() => {
       this.drawPose(ctx, canvas);
     }, 500);
