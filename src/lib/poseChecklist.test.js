c64b3512224efeb1a712e392e7168a7de648fd00
Aiden Gleave <aidengleave@gmail.com>
2019-03-29 15:05:28 +0000

    :ambulance: changed user message errors to new text

--- src/lib/poseChecklist.test.js
+++ src/lib/poseChecklist.test.js
@@ -14,8 +14,8 @@ it("#isWarrior2 returns message list for incorrect pose", () => {
   var input = sample["nearly"][0];
   var subject = new Pose(input);
   expect(subject.isWarrior2()[1]).toEqual([
-    [false, "Knee bent and stacked over foot"],
-    [true, "Right knee bent and stacked over right foot"]
+    [false, "Arms parallel to the ground"],
+    [true, "Knee bent and stacked over foot"]
   ]);
 });
 
@@ -23,7 +23,7 @@ it("#isWarrior2 returns message list for incorrect pose", () => {
   var input = sample["nearly"][1];
   var subject = new Pose(input);
   expect(subject.isWarrior2()[1]).toEqual([
-    [true, "Knee bent and stacked over foot"],
-    [false, "Right knee bent and stacked over right foot"]
+    [true, "Arms parallel to the ground"],
+    [false, "Knee bent and stacked over foot"]
   ]);
 });
